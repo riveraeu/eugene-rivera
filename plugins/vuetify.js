2cba672bf38375a5b1b406f483b20c137bf05db6
riveraeu <riveraeu@sonoma.edu>
2018-07-16 17:07:00 -0700

    first commit

---
+++ plugins/vuetify.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#2196F3',
+    secondary: '#4CAF50',
+    accent: '#FF9800',
+    error: '#b71c1c'
+  }
+})
